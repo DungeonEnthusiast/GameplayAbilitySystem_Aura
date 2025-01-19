f67ef60b45473ee9e1d4ba34f4c7220e576a3447
DungeonEnthusiast <rodneyraccoon1337@gmail.com>
2025-01-19 03:59:23 +0100

    Added Basic Progress Bar Widget

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright RE-Design
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright RE-Design
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
