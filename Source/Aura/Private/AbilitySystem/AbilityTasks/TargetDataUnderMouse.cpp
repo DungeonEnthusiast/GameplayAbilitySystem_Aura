298136d28b3a27afda4c777eb90ba7d24d67d8f3
DungeonEnthusiast <rodneyraccoon1337@gmail.com>
2025-02-04 06:00:39 +0100

    Added blueprint based on Anim Notifier - AN_FireBolt, Added new Gameplay Tag Event_Montage_FireBolt

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,10 @@
+// Copyright RE-Design
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
