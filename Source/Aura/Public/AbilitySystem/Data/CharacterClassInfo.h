8e6fe6472d86d2db95cd10843a30458b2b101bfc
DungeonEnthusiast <rodneyraccoon1337@gmail.com>
2025-04-26 21:42:25 +0200

    Setting up XP and Leveling Up system with Attribute and Spell point awards

--- Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/GameplayAbility.h"
+#include "ScalableFloat.h"
 #include "Engine/DataAsset.h"
 #include "CharacterClassInfo.generated.h"
 
@@ -28,6 +28,9 @@ struct FCharacterClassDefaultInfo
 
 	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
+	FScalableFloat XPReward = FScalableFloat();
 };
 
 /**
